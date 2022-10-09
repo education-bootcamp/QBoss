960107f2bb28016bb0f47f14199c512af976b4f1
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-10-09 21:13:21 +0530

    4

--- main.js
+++ main.js
@@ -9,13 +9,19 @@ letsGetStarted = () => {
         document.getElementById('level');
     setBorderColor(playerNameElement)
 
+    // _nimal____ trim() =>> nimal
     let tempPlayerName = playerNameElement.value;
     if (tempPlayerName.trim() === '') { // trim rid spaces _abc_ => after trim()==> abc
         playerNameElement.style.borderColor = 'red';
         alert('please insert name and level to continue!');
         return;
     }
-   //====>
+
+    playerName=tempPlayerName;
+    level=levelElement.value;
+    //====> redirect to gaming console==>
+
+
 }
 setBorderColor=(element)=>{
     element.style.borderColor='';
